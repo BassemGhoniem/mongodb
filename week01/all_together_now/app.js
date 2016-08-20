d5a1a2afab3bf0d9edcfb2d5bd4582168fbd3910
Bassem Ghoniem <bassem.ghoniem@yahoo.com>
2016-08-20 21:37:52 +0200

    week3 m101 done

--- week01/all_together_now/app.js
+++ week01/all_together_now/app.js
@@ -8,7 +8,7 @@ app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+MongoClient.connect('mongodb://localhost:27017/videos', function(err, db) {
 
     assert.equal(null, err);
     console.log("Successfully connected to MongoDB.");
@@ -24,7 +24,6 @@ MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
     app.use(function(req, res){
         res.sendStatus(404);
     });
-    
     var server = app.listen(3000, function() {
         var port = server.address().port;
         console.log('Express server listening on port %s.', port);
